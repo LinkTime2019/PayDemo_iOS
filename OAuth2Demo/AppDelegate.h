2d33f6c3529e5ad3f9c7d9464b2fff452c37fdaf
hechengjiu <1362611486@qq.com>
2019-02-21 15:09:54 +0800

    Initial Commit

---
+++ OAuth2Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OAuth2Demo
+//
+//  Created by ethereum on 2019/2/21.
+//  Copyright © 2019年 ethereum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OAuth2Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OAuth2Demo
+//
+//  Created by ethereum on 2019/2/21.
+//  Copyright © 2019年 ethereum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
